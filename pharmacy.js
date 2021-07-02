2e4338fa3dae3fc7a19f503df58901999f979961
Dayeon1351 <zzzz1351@gmail.com>
2021-07-02 23:31:24 +0900

    Fix typo

--- pharmacy.js
+++ pharmacy.js
@@ -1,16 +1,19 @@
 const PHARMACY_API_KEY = "a3b672b67e6841e09b8d2038b636b5f4";
 
-function getPharmacyInfo(){
-    fetch(`https://openapi.gg.go.kr/AnimalPharmacy?KEY=${PHARMACY_API_KEY}&Type=${"json"}&pIndex=${1}&pSize=${10}`
-    ).then(function(response){
-        return response.json();
-    }).then(function(json){
-        console.dir(json);
+function getPharmacyInfo() {
+  fetch(
+    `https://openapi.gg.go.kr/AnimalPharmacy?KEY=${PHARMACY_API_KEY}&Type=${"json"}&pIndex=${1}&pSize=${10}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.dir(json);
     });
 }
 
-function init(){
-    getPharmacyInfo();
+function init() {
+  getPharmacyInfo();
 }
 
 init();
